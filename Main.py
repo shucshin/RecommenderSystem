e72c6c66fc91a15c17e2c063a53942fe7751eb32
shucshin <robinucscs@gmail.com>
2024-04-16 22:28:48 -0600

    Champagne

--- Main.py
+++ Main.py
@@ -4,17 +4,10 @@ if __name__ == '__main__':
     Base = Funciones.leerBase()
     Cabe = Funciones.cabeceras()
 
-    # Solo para checar que lee el input bien
-    print("Base")
-    for x in Base:
-        print(x)
-    print("Cabeceras")
-    print(Cabe)
-
-    print("Favor de indicar su preferencia en la siguiente orden Marca,Talla,Color,Aumento(cm). Ejemplo: Nike,25,Negro,4") # Ejemplo: Nike,25,Negro,4
+    print("Favor de indicar su preferencia en la siguiente orden:\nUva, País, Año, Precio(Dólares).\nEjemplo: Pinot Noir, France, 2014, 70") 
     pref = input()
     datos = pref.split(',')
 
     match = Funciones.matchPreference(datos,Base)
-    print("El tennis que le recomendamos es: ")
+    print("El Champagne que le recomendamos es: ")
     print(match)
